afd668775fee884c2cadb5626c7ad51ac12ac04f
AdrjGo <gcadrianj@gmail.com>
2024-06-30 18:08:27 -0400

    Update Ejercicio3

--- Funciones/4.Arrays/Ejercicio3.js
+++ Funciones/4.Arrays/Ejercicio3.js
@@ -13,3 +13,32 @@ function longitudConsecutivos(n, x) {
 }
 console.log(longitudConsecutivos(7, -4));
 console.log(longitudConsecutivos(8, 6));
+
+//Otra solución WHILE
+function longitudConsecutivos2(n, x) {
+  let arreglo = [];
+  let i = 0;
+  while (i < n) {
+    let cuadrado = x ** 2;
+    arreglo.push(cuadrado);
+    i++;
+    x++;
+  }
+  return arreglo;
+}
+console.log(longitudConsecutivos2(7, -4));
+console.log(longitudConsecutivos2(8, 6));
+
+//Otra solución MAP
+function longitudConsecutivos3(n, x) {
+  return [...Array(n).keys()].map((i) => (x + i) ** 2);
+}
+console.log(longitudConsecutivos3(7, -4));
+console.log(longitudConsecutivos3(8, 6));
+
+//Otra solucióm FROM
+function longitudConsecutivos4(n, x) {
+  return Array.from({ length: n }, (_, i) => (x + i) ** 2);
+}
+console.log(longitudConsecutivos4(7, -4));
+console.log(longitudConsecutivos4(8, 6));
