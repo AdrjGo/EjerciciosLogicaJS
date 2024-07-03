45bdd696daf60b731ea98017e09e1273e52aa6a8
Adrian Jesus Gomez Catacora <161991557+AdrjGo@users.noreply.github.com>
2024-07-02 21:27:55 -0400

    Update Ejercicio2 Arrays

--- Funciones/4.Arrays/Ejercicio2.js
+++ Funciones/4.Arrays/Ejercicio2.js
@@ -2,7 +2,7 @@
 Dado un número natural n, regresar el arreglo de longitud n cuyos elementos son los cuadrados de los primeros números naturales.
 Entrada: 5. Salida: [0, 1, 4, 9, 16].
 Entrada: 20. Salida: [  0, 1, 4, 9, 16, 25, 36, 49, 64, 81, 100, 121, 144, 169, 196, 225, 256, 289, 324, 361].
-*/
+*/ 
 function longitudCuadrados(n) {
   arreglo = [];
   for (let i = 0; i < n; i++) {
