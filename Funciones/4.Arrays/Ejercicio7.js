5320df9d27595d892d3c4dc582fbefec7a0caff2
AdrjGo <gcadrianj@gmail.com>
2024-07-02 20:21:50 -0400

    Update Ejercicio7

--- Funciones/4.Arrays/Ejercicio7.js
+++ Funciones/4.Arrays/Ejercicio7.js
@@ -5,11 +5,8 @@ Entrada: 4, 20, −15. Salida: [20, 5, −10, −25].
 Entrada: 0, 5, −3. Salida: [].
 */
 function progresion(n, a, b) {
-  arreglo = [];
-  if (n > 0) {
-    arreglo.push(a);
-  }
-
+  let arreglo = [];
+  if (n > 0) arreglo.push(a);
   for (let i = 1; i < n; i++) {
     let elementos = (a += b); //a = a + b
     arreglo.push(elementos);
@@ -19,3 +16,45 @@ function progresion(n, a, b) {
 console.log(progresion(5, -3, 11));
 console.log(progresion(4, 20, -15));
 console.log(progresion(0, 5, -3));
+
+//Otra solución WHILE
+function progresion2(n, a, b) {
+  let arreglo = [];
+  let i = 1;
+  if (n > 0) arreglo.push(a);
+  while (i < n) {
+    let elementos = (a += b);
+    arreglo.push(elementos);
+    i++;
+  }
+  return arreglo;
+}
+console.log(progresion2(5, -3, 11));
+console.log(progresion2(4, 20, -15));
+console.log(progresion2(0, 5, -3));
+
+//Otra solución MAP
+function progresion3(n, a, b) {
+  return [...Array(n).keys()].map((i) => (i === 0 ? a : (a += b)));
+}
+console.log(progresion3(5, -3, 11));
+console.log(progresion3(4, 20, -15));
+console.log(progresion3(0, 5, -3));
+
+//Otra solución con FILL y MAP
+function progresion4(n, a, b) {
+  return Array(n)
+    .fill(0)
+    .map((_, i) => a + i * b);
+}
+console.log(progresion4(5, -3, 11));
+console.log(progresion4(4, 20, -15));
+console.log(progresion4(0, 5, -3));
+
+//Otra solución FROM
+function progresion5(n, a, b) {
+  return Array.from({ length: n }, (_, i) => a + i * b);
+}
+console.log(progresion5(5, -3, 11));
+console.log(progresion5(4, 20, -15));
+console.log(progresion5(0, 5, -3));
