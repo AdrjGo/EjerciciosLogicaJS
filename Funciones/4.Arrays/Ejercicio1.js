fd18d4ebb2e74a05e556b837d2e90238f484f766
Adrian Jesus Gomez Catacora <161991557+AdrjGo@users.noreply.github.com>
2024-07-02 21:27:11 -0400

    Update Ejercicio1 Arrays

--- Funciones/4.Arrays/Ejercicio1.js
+++ Funciones/4.Arrays/Ejercicio1.js
@@ -2,7 +2,7 @@
 Dado un número natural n y un número ENTERO v, regresar el arreglo de longitud n tal que todas sus entradas son iguales a v. 
 En otras palabras, construir un arreglo constante de longitud dada y con el valor dado.
 Entrada: 6, −14. Salida: [−14, −14, −14, −14, −14, −14].
-*/
+*/ 
 function longitud(n, v) {
   arreglo = [];
   for (let i = 0; i < n; i++) {
