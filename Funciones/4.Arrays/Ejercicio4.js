7d488c074c6928934c281a63cbb68557d6332ef6
Adrian Jesus Gomez Catacora <161991557+AdrjGo@users.noreply.github.com>
2024-07-02 21:28:14 -0400

    Update Ejercicio4 Arrays

--- Funciones/4.Arrays/Ejercicio4.js
+++ Funciones/4.Arrays/Ejercicio4.js
@@ -1,6 +1,6 @@
 /*
 Dado un número natural n, regresar el arreglo a de longitud n cuyos elementos son las primeras potencias de 2, a partir de la potencia 0. 
-Entrada: 5. Salida: [1, 2, 4, 8, 16].
+Entrada: 5. Salida: [1, 2, 4, 8, 16]. 
 */
 function potencia(n) {
   let arreglo = [];
