ebb41c8980d961fd7bb3a0694397a301fb19348d
Adrian Jesus Gomez Catacora <161991557+AdrjGo@users.noreply.github.com>
2024-07-02 21:40:27 -0400

    Update Ejercicio8 Arrays

--- Funciones/4.Arrays/Ejercicio8.js
+++ Funciones/4.Arrays/Ejercicio8.js
@@ -2,7 +2,7 @@
 Dado un arreglo a de números enteros, regresar un arreglo de números enteros de la misma longitud,
 cuyos elementos son los cuadrados de los elementos del arreglo dado.
 Entrada: [−12, 23, 14, 6]. Salida: [144, 529, 196, 36].
-*/ 
+*/
 function arrayCuadrado(array) {
   let arreglo = [];
   for (let i = 0; i < array.length; i++) {
@@ -13,8 +13,21 @@ function arrayCuadrado(array) {
 }
 console.log(arrayCuadrado([-12, 23, 14, 6]));
 
-//Otra solución WHILE
+
+//Otra solución FOR IN
 function arrayCuadrado2(array) {
+  let arreglo = [];
+  for (let i in array) {
+    let elementos = array[i] ** 2;
+    arreglo.push(elementos);
+  }
+  return arreglo;
+}
+console.log(arrayCuadrado2([-12, 23, 14, 6]));
+
+
+//Otra solución WHILE
+function arrayCuadrado3(array) {
   let arreglo = [];
   let i = 0;
   while (i < array.length) {
@@ -24,10 +37,11 @@ function arrayCuadrado2(array) {
   }
   return arreglo;
 }
-console.log(arrayCuadrado2([-12, 23, 14, 6]));
+console.log(arrayCuadrado3([-12, 23, 14, 6]));
+
 
 //Otra solución FOREACH
-function arrayCuadrado3(array) {
+function arrayCuadrado4(array) {
   let arreglo = [];
   array.forEach((_, i) => {
     let elementos = array[i] ** 2;
@@ -35,25 +49,28 @@ function arrayCuadrado3(array) {
   });
   return arreglo;
 }
-console.log(arrayCuadrado3([-12, 23, 14, 6]));
+console.log(arrayCuadrado4([-12, 23, 14, 6]));
+
 
 //Otra solición MAP
-function arrayCuadrado4(array) {
+function arrayCuadrado5(array) {
   let elementos = array.map((i) => i ** 2);
   return elementos;
 }
-console.log(arrayCuadrado4([-12, 23, 14, 6]));
+console.log(arrayCuadrado5([-12, 23, 14, 6]));
+
 
 //Otra solución FILL y MAP
-function arrayCuadrado5(array) {
+function arrayCuadrado6(array) {
   return Array(array.length)
     .fill(0)
     .map((_, i) => array[i] ** 2);
 }
-console.log(arrayCuadrado5([-12, 23, 14, 6]));
+console.log(arrayCuadrado6([-12, 23, 14, 6]));
+
 
 //Otra solución FROM
-function arrayCuadrado6(array) {
+function arrayCuadrado7(array) {
   return Array.from(array, (i) => i ** 2);
 }
-console.log(arrayCuadrado6([-12, 23, 14, 6]));
+console.log(arrayCuadrado7([-12, 23, 14, 6]));
