7143289dcf2c9c7cc678ce435036c0e95be4d2e5
Adrian Jesus Gomez Catacora <161991557+AdrjGo@users.noreply.github.com>
2024-07-02 21:28:38 -0400

    Update Ejercicio6 Arrays

--- Funciones/4.Arrays/Ejercicio6.js
+++ Funciones/4.Arrays/Ejercicio6.js
@@ -1,7 +1,7 @@
 /*
 Dado un número natural n, regresar el arreglo de longitud n cuyos elementos son los números 1 (en posiciones pares) y −1 (en posiciones impares).
 Entrada: 5. Salida: [1, −1, 1, −1, 1].
-*/
+*/ 
 function parImpar(n) {
   arreglo = [];
   for (let i = 0; i < n; i++) {
