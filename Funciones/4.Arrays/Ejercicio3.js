ac81293fd50f9ab9161960157753839d193c41be
Adrian Jesus Gomez Catacora <161991557+AdrjGo@users.noreply.github.com>
2024-07-02 21:28:05 -0400

    Update Ejercicio3 Arrays

--- Funciones/4.Arrays/Ejercicio3.js
+++ Funciones/4.Arrays/Ejercicio3.js
@@ -2,7 +2,7 @@
 Dados un número natural n y un número entero x, regresar el arreglo de longitud n cuyos elementos son los cuadrados de los enteros consecutivos, a partir de x.
 Entrada: 7, −4. Salida: [16, 9, 4, 1, 0, 1, 4].
 Entrada: 8, 6. Salida: [ 36, 49, 64, 81, 100, 121, 144, 169 ].
-*/
+*/ 
 function longitudConsecutivos(n, x) {
   arreglo = [];
   for (let i = 0; i < n; i++, x++) {
